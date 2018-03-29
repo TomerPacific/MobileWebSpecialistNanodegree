6eb9e984db9dc598eb5248bc7404fab5c95321f2
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-03-29 23:27:12 +0200

    Converted article call to ajax

--- Dynamic And Offline Capable Web Apps/Lesson 02 - Ajax with JQuery/Project/app.js
+++ Dynamic And Offline Capable Web Apps/Lesson 02 - Ajax with JQuery/Project/app.js
@@ -18,10 +18,12 @@
     }).done(addImage);
 
 
-    const articleRequest = new XMLHttpRequest();
-    articleRequest.onload = addArticles;
-    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=635d2676d9d248b48605a5eac3ea3b43`);
-    articleRequest.send();
+   $.ajax({
+    url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}`
+    headers:{
+        Authorization: 'API-KEY 635d2676d9d248b48605a5eac3ea3b43'     
+        }
+    }).done(addArticles);
 
     function addImage(images) {
         const firstImage = images.results[0];
@@ -32,12 +34,11 @@
     }
 
 
-    function addArticles () {
-         let htmlContent = '';
-         let data = JSON.parse(this.responseText);
-         let docs = data.response.docs;
+    function addArticles (articles) {
 
-         if(docs && docs.length > 1) {
+         if(articles.response && article.response.docs && article.response.docs.length > 1) {
+            const data = articles.response.docs;
+            
             htmlContent = '<ul>' + docs.map(article => '<li class="article"><h2><a href="${article.web_url}">${article.headline.main}</a></h2> <p>${article.snippet}</p></li>').join('') + '</ul>';
          } else {
             htmlContent = '<div class="error-no-articles">No articles available</div>';
