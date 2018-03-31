b29ee6290d72f8a7d399b47ba8dac4d7ee8591ec
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-03-31 22:30:37 +0200

    Converted unsplash request to use fetch

--- Dynamic And Offline Capable Web Apps/Lesson 03 - Ajax with Fetch/Project/app.js
+++ Dynamic And Offline Capable Web Apps/Lesson 03 - Ajax with Fetch/Project/app.js
@@ -10,12 +10,14 @@
         searchedForText = searchField.value;
     });
     
-   $.ajax({
-    url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`
-    headers:{
-        Authorization: 'Client-ID 123abc456def'     
-        }
-    }).done(addImage);
+  fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+    headers: {
+        Authorization: 'Client-ID abc123'
+    }
+    }).then(response => response.json())
+    .then(addImage)
+    .catch(e => requestError(e, 'image'));
+
 
 
    $.ajax({
@@ -25,12 +27,20 @@
         }
     }).done(addArticles);
 
-    function addImage(images) {
-        const firstImage = images.results[0];
-        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
-            <img src="${firstImage.urls.small}" alt="${searchedForText}">
-            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-        </figure>`);
+    function addImage(data) {
+        let htmlContent = '';
+        const firstImage = data.results[0];
+
+        if (firstImage) {
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = 'Unfortunately, no image was returned for your search.'
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
 
 
@@ -45,6 +55,12 @@
          }
 
           responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
     }
 
 })();
